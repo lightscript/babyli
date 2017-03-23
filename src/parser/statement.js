15d0f98a6bda6a452b4642ca064820ede58be4d5
William C. Johnson <wcjohnson@oigroup.net>
2017-03-23 15:05:07 -0400

    Update to `parseIfStatement`: don’t care about matching block types, just no newlines after else

--- src/parser/statement.js
+++ src/parser/statement.js
@@ -340,26 +340,19 @@ pp.parseFunctionStatement = function (node) {
   return this.parseFunction(node, true);
 };
 
-pp.parseIfStatement = function (node, mustBeWhiteBlock) {
+pp.parseIfStatement = function (node) {
   this.next();
   node.test = this.parseParenExpression();
-  const isWhiteBlock = this.match(tt.colon);
-  if (mustBeWhiteBlock && !isWhiteBlock) this.unexpected(null, tt.colon);
   node.consequent = this.parseStatement(false);
+
   if (this.hasPlugin("lightscript") && this.match(tt._elif)) {
-    node.alternate = this.parseIfStatement(this.startNode(), isWhiteBlock);
+    node.alternate = this.parseIfStatement(this.startNode());
   } else {
     if (this.eat(tt._else)) {
-      if (this.hasPlugin("lightscript")) {
-        if (this.match(tt._if)) {
-          node.alternate = this.parseIfStatement(this.startNode(), isWhiteBlock);
-        } else {
-          if (isWhiteBlock && !(this.match(tt.colon))) this.unexpected(null, tt.colon);
-          node.alternate = this.parseStatement(false);
-        }
-      } else {
-        node.alternate = this.parseStatement(false);
+      if (this.hasPlugin("lightscript") && this.isLineBreak()) {
+        this.unexpected(this.state.lastTokEnd, tt.colon);
       }
+      node.alternate = this.parseStatement(false);
     } else {
       node.alternate = null;
     }
